90249bf251133d4de3dec46e04308dea8f289b92
diodonfrost <diodon.frost@diodonfrost.me>
2019-01-23 18:22:00 +0100

    change sshd service name for debian family

--- tests/inspec/bastion_spec.rb
+++ tests/inspec/bastion_spec.rb
@@ -5,9 +5,8 @@ sshd_service = 'sshd'
 control 'bastion-01' do
   impact 1.0
   title 'sshd service'
-  desc 'sshd service should be enabled and started'
+  desc 'sshd service should be started'
   describe service(sshd_service) do
-    it { should be_enabled }
     it { should be_running }
   end
 end
